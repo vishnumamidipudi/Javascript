5cf925b13efffcc08b00ba72287b2b2f74c28a1c
vishnumamidipudi <44763227+vishnumamidipudi@users.noreply.github.com>
2019-02-19 18:35:52 +0530

    Add files via upload
    Basics of p5.js. Drawing shapes and using colours

---
+++ p5js/Deadpan.js
@@ -0,0 +1,26 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(0);
+	rectMode(CENTER);
+	fill(255,0,0);
+	ellipse(200, 200, 400, 400);
+	fill(0,0,255);
+	rect(200, 200, 300, 300);
+	fill(200,0,0);
+	ellipse(200, 200, 300, 300);
+	fill(0,0,200);
+	rect(200, 200, 200, 200);
+	fill(150,0,0);
+	ellipse(200, 200, 200, 200);
+	fill(0,0,100);
+	rect(200, 200, 100, 100);
+	fill(100,0,0);
+	ellipse(200, 200, 100, 100);
+	fill(0,0,500);
+	rect(200, 200, 50, 50);
+	fill(10,0,0);
+	ellipse(200, 200, 50, 50);
+}
